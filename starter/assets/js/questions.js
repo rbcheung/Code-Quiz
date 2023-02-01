a682fb52a74aedda0298182b1e75639d306d1b1b
Ryan Cheung <ryan_cheung@hotmail.co.uk>
2023-02-01 22:56:20 +0000

    Majority of highscores JS complete. README updated. Few errors to fix next

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,3 +1,6 @@
+
+// All variables 
+
 var startBtn = document.querySelector("#start");
 //console.log(startBtn);
 var timer = document.querySelector("#time");
@@ -20,44 +23,43 @@ var time = document.getElementById("time");
 //console.log(time);
 var feedback = document.getElementById("feedback")
 //console.log("feedback")
-var initials = document.getElementById("initials")
-//console.log("feedback")
 
 var q;
 var runningQuestionIndex = 0;
 var score = 0;
-var secondsLeft = 60;
+var secondsLeft = 75;
 var timerInterval;
+var finalScore;
 
 // Questions Array
 var questions = [
     {
         question: "Javascript is an _______ language?",
-        correct: "Object-Orientated",
+        correct: "1.Object-Orientated",
         answers: [
-            { choiceA: "Object-Orientated"},
-            { choiceB: "Object-Based"},
-            { choiceC: "Procedural"},
+            { choiceA: "1.Object-Orientated"},
+            { choiceB: "2.Object-Based"},
+            { choiceC: "3.Procedural"},
         ]
 
     },
     {
         question: "Which of the following is not a framework?",
-        correct: "jQuery",
+        correct: "1.jQuery",
         answers: [
-            { choiceA: "jQuery"},
-            { choiceB: "Cocoa JS"},
-            { choiceC: " JavaScript"}
+            { choiceA: "1.jQuery"},
+            { choiceB: "2.Cocoa JS"},
+            { choiceC: "3.JavaScript"}
         ]
 
     },
     {
         question: "Which of the following is the property that is triggered in response to JS errors?",
-        correct: "onerror",
+        correct: "2.onerror",
         answers: [
-            { choiceA: "onclick"},
-            { choiceB: "onerror"},
-            { choiceC: "onmessage"},
+            { choiceA: "1.onclick"},
+            { choiceB: "2.onerror"},
+            { choiceC: "3.onmessage"},
         ]
 
     },
@@ -74,9 +76,10 @@ var questions = [
 function setTime() {
         timerInterval = setInterval(function(){
         secondsLeft--;
-        time.textContent = secondsLeft
+        time.textContent = secondsLeft;
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
+            quizEnd();
         }
 
     }, 1000);
@@ -86,7 +89,7 @@ function quizEnd() {
     
     var endScreen = document.getElementById("end-screen");
     endScreen.removeAttribute("class");
-    var finalScore = document.getElementById("final-score");
+    finalScore = document.getElementById("final-score");
     finalScore.textContent = score;
     questionsContainer.setAttribute("class", "hide");
     clearInterval(timerInterval);
@@ -109,14 +112,14 @@ function startGame() {
 }
 
 function showQuestion() {
-     console.log(runningQuestionIndex)
+    //  console.log(runningQuestionIndex)
     if (runningQuestionIndex === 3) {
         quizEnd();
     }
     else {
         q = questions[runningQuestionIndex];
         // questionElement.innerText = questions[0].question1;
-        questionElement.innerText = q.question
+        questionElement.innerText = q.question;
         // console.log(q.question)
         answerA.innerText = q.answers[0].choiceA;
         // console.log(q.answers[0]);
@@ -129,17 +132,17 @@ function showQuestion() {
 
 answerA.addEventListener('click', function(){
     if (q.correct === answerA.textContent) {
-        console.log(q.correct)
+        // console.log(q.correct)
         //console.log(answerA.textContent)
         score++;
-        feedback.style.display = "block"
-        feedback.textContent = "Correct!"
+        feedback.style.display = "block";
+        feedback.textContent = "Correct!";
         runningQuestionIndex++;
         showQuestion();
    }
    else {
-    feedback.style.display = "block"
-    feedback.textContent = "Incorrect!"
+    feedback.style.display = "block";
+    feedback.textContent = "Incorrect!";
     time.textContent = secondsLeft -= 5;
     runningQuestionIndex++;
     showQuestion();
@@ -149,93 +152,30 @@ answerA.addEventListener('click', function(){
 })
 answerB.addEventListener('click', function(){
     if (q.correct === answerB.textContent) {
-        console.log(q.correct)
+        // console.log(q.correct)
         //console.log(answerB.textContent)
         score++;
-        feedback.style.display = "block"
-        feedback.textContent = "Correct!"
+        feedback.style.display = "block";
+        feedback.textContent = "Correct!";
         runningQuestionIndex++;
         showQuestion();
    }
    else {
-    feedback.style.display = "block"
-    feedback.textContent = "Incorrect!"
+    feedback.style.display = "block";
+    feedback.textContent = "Incorrect!";
     time.textContent = secondsLeft -= 5;
     runningQuestionIndex++;
-    showQuestion()
+    showQuestion();
 
    }
 
 })
 answerC.addEventListener('click', function(){
-    feedback.style.display = "block"
-    feedback.textContent = "Incorrect!"
+    feedback.style.display = "block";
+    feedback.textContent = "Incorrect!";
     time.textContent = secondsLeft -= 5;
     runningQuestionIndex++;
     showQuestion();
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// For loop to loop over the answerElement variable as it is a node list and acts like an array.
-// Added Event Listener to listen for a click on the answerElement variable.
-// If event target clicked == answerA, add score++
-// If the event target clicked == answerA, then create new paragraph and append it to the body.
-// Then I changed the content of the correctP variable to say "You have chosen the correct answer!!!"
-// else any other button is clicked, I do the same thing but say "You have chosen the wrong answer!!!" instead.
-// else event target clicked == answerA, runningQuestionIndex++ and shoqQuestion to get new questions.
-// else event target clicked == answerA, set the text of the time variable to equal secondsLeft -= 5 seconds.
-
-//function checkAnswer() {
-
-    
-    // for (i = 0; i < answerElement.length; i++) {
-    //     console.log(answerElement[i]);
-    //     answerElement[i].addEventListener("click", function (e) {
-    //         if (e.target.matches("#A")) {
-    //             score++;
-    //             feedback.removeAttribute("class");
-    //             feedback.textContent = "Correct!";
-    //             runningQuestionIndex++;
-    //             showQuestion();
-    //         }
-    //         else {
-    //             feedback.removeAttribute("class");
-    //             feedback.textContent = "Incorrect!";
-    //             time.textContent = secondsLeft -= 5;
-    //             showQuestion();
-    //         }
-
-    //     })
-    // }
-
-//}
 
---
+++ starter/assets/js/scores.js
@@ -0,0 +1,55 @@
+var initials = document.querySelector("#initials");
+// console.log(initials);
+var submitBtn = document.querySelector("#submit");
+// console.log(submitBtn);
+var highscoresList = document.querySelector("#highscores");
+// console.log(highscoresList);
+var endScreen = document.querySelector("#end-screen");
+// console.log(endScreen);
+var finalScore = document.querySelector("#final-score");
+// console.log(finalScore);
+var clearScore = document.querySelector("#clear");
+
+if(submitBtn) {
+  submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    // create user object from submission
+    var user = {
+      Initials: initials.value.trim(),
+      Score: finalScore.textContent
+
+    };
+    // console.log(user);
+    
+    if (user.Initials === "") {
+      alert("Please input initials.");
+    } 
+    else {
+      alert("Thanks for playing.");
+  
+      localStorage.setItem("user", JSON.stringify(user));
+      location.href = "highscores.html";
+
+     
+    }
+  });
+}
+
+var lastUser = JSON.parse(localStorage.getItem("user"));
+// console.log(lastUser)
+var newLi = document.createElement("li");
+newLi.textContent = lastUser.Initials + " " + " -" + " " + lastUser.Score;
+// console.log(highscoresList)
+highscoresList.append(newLi);
+// console.log(newLi)
+
+
+clearScore.addEventListener("click", function(){
+localStorage.clear();
+newLi.classList.add("hide");
+})
+
+
+
+
